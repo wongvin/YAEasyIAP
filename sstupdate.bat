4e2e967a2052da0f4e08fff3219f5a409dc55e5a
Vincent Wong <hk.vwong@gmail.com>
2024-01-21 10:24:28 -0700

    Update batch file to close window with countdown

--- sstupdate.bat
+++ sstupdate.bat
@@ -1,11 +1,22 @@
 @echo off
 
-set _hex=binary.hex
+set _hex=file name.hex
+
 set _cmd=reg query HKLM\HARDWARE\DEVICEMAP\SERIALCOMM
 
 for /f "tokens=3" %%p in ('%_cmd% ^| findstr /r /c:"VCP[0-9]"') do (
 	for /f "tokens=2 delims=M" %%n in ("%%p") do (
 		echo %%p found
+		echo yaeasyiap.exe %%n "%_hex%"
+		echo:
 		call yaeasyiap.exe %%n "%_hex%"
+		goto finish
 	)
 )
+
+echo COM Port not found
+
+:finish
+echo:
+echo This window will close automatically after the countdown.
+timeout /t 180 /nobreak
